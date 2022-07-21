26542cb1f1f04cbb436eea175b3bf568bf9c0cb3
Ivan Cheah <69534752+IvanCYX@users.noreply.github.com>
2022-07-21 14:47:19 -0500

    Able to insert new components, working on database fix and string handling

--- src/database.h
+++ src/database.h
@@ -12,7 +12,8 @@ extern "C" {
     DLLExport int __cdecl query_people(int session_id, const char* schema, int* n_people, char* username , char* name, char* full_name, char* email, char* institute, char* timezone);
     DLLExport int __cdecl query_specific_password(int session_id, const char* schema, const char* username, const char* password);
     DLLExport int __cdecl query_component(int session_id, const char* schema, const char* part, int* n_component, int* id, char* status, char* description, char* serial_number, char* location);
-    DLLExport int __cdecl insert_component(int session_id, const char* schema, char* part, char* status, char* description, char* location);
+    DLLExport int __cdecl insert_component(int session_id, const char* schema, const char* part, int version, const char* status, const char* description, const char* location);
+    DLLExport int __cdecl update_component(int session_id, const char* schema, const char* status, int parent);
     DLLExport int __cdecl disconnect(int session_id);
 }
 
