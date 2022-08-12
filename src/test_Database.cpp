7fd2b0049fbbc1a5f856bda2db43b3f9f66d6c14
Caleb Fangmeier <caleb@fangmeier.tech>
2022-08-12 17:14:51 -0500

    Further cleanup, adds functions to handle transactions, and save test data.

--- src/test_Database.cpp
+++ src/test_Database.cpp
@@ -17,64 +17,97 @@ using namespace std;
 bool VERBOSE = false;
 
 int test_update_component(int session_id, const char* schema) {
-    int id;
-    string status;
-    int parent;
 
-    cout << "Enter id" << endl;
-    cin >> id;
-    cout << "Enter status" << endl;
-    cin.ignore();
-    getline(cin, status);
-    cout << "Enter parent" << endl;
-    cin >> parent;
+    int comp_id = insert_component(session_id, schema, "test_part", 1,
+                                   "test", "This is a test component", "Mr. Test",
+                                   "TEST-001", "testsville");
 
-    update_component(session_id, schema, id, status.c_str(), parent);
-    return 0;
+    str_table_t status;
+    str_table_t location;
+    str_table_t description;
+    str_table_t serial_number;
+    int_table_t id;
+    int n_component;
+
+    update_component(session_id, schema, comp_id, "updated", 2);
+    query_components(session_id, schema, "test_part", &n_component, (int *) id,
+                     (char *) status, (char *) description, (char *) serial_number, (char *) location);
+    remove_component(session_id, schema, comp_id);
+
+    bool success = false;
+    for(int i=0; i<n_component; i++) {
+        string comp_status = status[i];
+        if (VERBOSE) {
+            bool match = (comp_status == "updated");
+            cout << "\"" << comp_status << R"(" == "updated")" << "? " << match << endl;
+        }
+        if (id[i] == comp_id && comp_status == "updated") {
+            success = true;
+            break;
+        }
+    }
+    return success ? 0 : 1;
 }
 
 int test_query_complaint(int session_id, const char* schema) {
     int n_complaint;
-    char who[128][128];
-    char description[128][128];
-    int id[128];
+    str_table_t who;
+    str_table_t description;
+    int_table_t id;
 
     query_complaint(session_id, schema, &n_complaint, (char*) who, (char*) description, (int*) id);
-    cout << n_complaint << endl;
-    for(int i=0; i<n_complaint; i++) {
-        cout << (char*) who[i] << ", " << (char*) description[i] <<  ", " << id[i] << endl;
+    if (VERBOSE) {
+        cout << endl;
+        for(int i=0; i<n_complaint; i++) {
+            cout << who[i] << ", " << description[i] <<  ", " << id[i] << endl;
+        }
     }
-    return 0;
+    return n_complaint > 0 ? 0 : 1;
 }
 
 int test_query_parts(int session_id, const char* schema) {
     int n_parts;
-    char parts[128][128];
-    int versions[128];
-    query_parts(session_id, schema, &n_parts, (char*) parts, (int*) versions);
-    cout << n_parts << endl;
-    for(int i=0; i<n_parts; i++) {
-        cout << (char*) parts[i] << ", " << versions[i] << endl;
+    str_table_t part;
+    int_table_t version;
+    str_table_t description;
+    str_table_t prefix;
+    float_table_t dim_x;
+    float_table_t dim_y;
+    float_table_t dim_z;
+    str_table_t type;
+    query_parts(session_id, schema, &n_parts, (char*) part, (int*) version, (char*) description, (char*) prefix,
+                (float*) dim_x, (float*) dim_y,(float*) dim_z, (char*) type);
+    if (VERBOSE) {
+        for(int i=0; i<n_parts; i++) {
+            cout << (char*) part[i] << ", " << version[i] << ", " << description[i]<< ", " << prefix[i] << endl;
+        }
     }
-
-    return 0;
+    return n_parts > 0 ? 0 : 1;
 }
 
 int test_query_people(int session_id, const char* schema) {
     int n_people;
-    char username[128][128];
-    char name[128][128];
-    char full_name[128][128];
-    char email[128][128];
-    char institute[128][128];
-    char timezone[128][128];
+    str_table_t username;
+    str_table_t name;
+    str_table_t full_name;
+    str_table_t email;
+    str_table_t institute;
+    str_table_t timezone;
 
     query_people(session_id, schema, &n_people, (char*) username , (char*) name,
                  (char*) full_name, (char*) email, (char*) institute, (char*) timezone);
-    for(int i=0; i<n_people; i++) {
-        cout << (char*) username[i] << ", " << (char*) name[i] << ", " << (char*) full_name[i] << ", " << (char*) email[i] << ", " << (char*) institute[i] << ", " << (char*) timezone[i] << endl;
+    if (VERBOSE) {
+        cout << endl;
+        for(int i=0; i<n_people; i++) {
+            cout << username[i]
+            << ", " << name[i]
+            << ", " << full_name[i]
+            << ", " << email[i]
+            << ", " << institute[i]
+            << ", " << timezone[i] << endl;
+        }
     }
-    return 0;
+    return n_people > 0 ? 0 : 1;
 }
 
 int test_check_login(int session_id, const char* schema, const char* username, const char* password) {
@@ -83,11 +116,11 @@ int test_check_login(int session_id, const char* schema, const char* username, c
 }
 
 int test_query_components(int session_id, const char* schema, const char* part) {
-    char status[128][128];
-    char location[128][128];
-    char description[128][128];
-    char serial_number[128][128];
-    int id[128];
+    str_table_t status;
+    str_table_t location;
+    str_table_t description;
+    str_table_t serial_number;
+    int_table_t id;
     int n_component;
 
     query_components(session_id, schema, part, &n_component, (int*) id ,
@@ -115,11 +148,11 @@ int test_insert_component(int session_id, const char* schema) {
     const char* test_serial_number = "TEST-001";
     const char* test_location = "testsville";
 
-    char status[128][128];
-    char location[128][128];
-    char description[128][128];
-    char serial_number[128][128];
-    int id[128];
+    str_table_t status;
+    str_table_t location;
+    str_table_t description;
+    str_table_t serial_number;
+    int_table_t id;
     int n_component_pre_insert, n_component_post_insert;
 
     query_components(session_id, schema, test_part, &n_component_pre_insert, (int*) id ,
@@ -151,11 +184,11 @@ int test_remove_component(int session_id, const char* schema) {
     const char* test_serial_number = "TEST-001";
     const char* test_location = "testsville";
 
-    char status[128][128];
-    char location[128][128];
-    char description[128][128];
-    char serial_number[128][128];
-    int id[128];
+    str_table_t status;
+    str_table_t location;
+    str_table_t description;
+    str_table_t serial_number;
+    int_table_t id;
     int n_component_pre_remove, n_component_post_remove;
 
     query_components(session_id, schema, test_part, &n_component_pre_remove, (int*) id ,
@@ -187,24 +220,16 @@ int test_remove_component(int session_id, const char* schema) {
 
 
 int test_insert_log(int session_id, const char* schema) {
-    string userid;
-    string remote_ip;
-    string type;
-
-    cout << "Enter userID" << endl;
-    getline(cin, userid);
-    cout << "Enter remote IP" << endl;
-    getline(cin, remote_ip);
-    cout << "Enter type" << endl;
-    getline(cin, type);
-
+    string userid = "test user";
+    string remote_ip = "1.2.3.4";
+    string type = "test log";
 
     insert_log(session_id, schema, userid.c_str(), remote_ip.c_str(), type.c_str());
     return 0;
 }
 
 int test_get_schemas(int session_id) {
-    char schemas[128][128];
+    str_table_t schemas;
     int n_schemas;
     get_schemas(session_id, &n_schemas, (char*) schemas);
 
@@ -217,6 +242,14 @@ int test_get_schemas(int session_id) {
     return n_schemas > 0 ? 0 : 1;
 }
 
+int test_insert_test(int session_id, const char* schema) {
+    string userid = "test user";
+    string remote_ip = "1.2.3.4";
+    string type = "test log";
+
+    insert_test(session_id, schema, "This is a test test", "dx=5,dy=12,dz=92,t=1h23m,etc,etc", 68, "test");
+    return 0;
+}
 
 int main(int argc, const char** argv) {
     VERBOSE = false;
@@ -229,11 +262,12 @@ int main(int argc, const char** argv) {
     TEST(test_query_components, session_id, "cmsfpix_phase2", "rd53a_chip");
     TEST(test_insert_component, session_id, "cmsfpix_phase2");
     TEST(test_remove_component, session_id, "cmsfpix_phase2");
-//    test_insert_log(session_id, "cmsfpix_phase2");
-//    test_query_complaint(session_id, "cmsfpix_phase2");
-//    test_query_parts(session_id, "cmsfpix_phase2");
-//    test_query_people(session_id, "cmsfpix_phase2");
-//    test_update_component(session_id, "cmsfpix_phase2");
+    TEST(test_insert_log, session_id, "cmsfpix_phase2");
+    TEST(test_query_complaint, session_id, "cmsfpix_phase2");
+    TEST(test_query_parts, session_id, "cmsfpix_phase2");
+    TEST(test_query_people, session_id, "cmsfpix_phase2");
+    TEST(test_update_component, session_id, "cmsfpix_phase2");
+    TEST(test_insert_test, session_id, "cmsfpix_phase2");
 
     disconnect(session_id);
     return 0;
