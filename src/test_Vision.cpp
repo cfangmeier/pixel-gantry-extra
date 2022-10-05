7c98e79e826cef2001d296be3626c2fc5361fba4
Caleb Fangmeier <caleb@fangmeier.tech>
2022-10-05 12:05:22 -0500

    Fixes missing load of parameter file for ueye driver

--- src/test_Vision.cpp
+++ src/test_Vision.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include "vision.h"
 
 using namespace std;
 
 int main(int argc, const char** argv) {
+    int n_cameras;
+    int serial_numbers[10];
+    int vendor_ids[10];
+//    get_cameras(&n_cameras, serial_numbers, vendor_ids);
     return 0;
 }
